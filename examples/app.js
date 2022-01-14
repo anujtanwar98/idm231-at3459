93338079b025606d6225efb160968aeb0f7c9c66
Anuj <at3459@drexel.edu>
2022-01-13 19:51:41 -0500

    Examples
    variables, Strings etc

---
+++ examples/app.js
@@ -0,0 +1,39 @@
+var principle = "Anuj Tanwar";
+var teacher = "Jordan";
+console.log(principle);
+
+/* use either one of this with or without _
+var gymTeacher = "Brett";
+var the_best_gym_teacher = "";*/
+
+console.log("This is a string");
+var total = "20"; 
+/* this is a string with ""*/
+var value1 = 20;
+var value2 = 10;
+
+var multiplyValue = value1 * value2;
+var addValue = value1 + value2;
+var minusValue = value1 - value2;
+var divideValue = value1 / value2;
+console.log(multiplyValue);
+console.log(addValue);
+console.log(minusValue);
+console.log(divideValue);
+
+var age= 17;
+var drinkingLimit = 21;
+// Stmple if statement 
+if (age > drinkingLimit) {
+console.log("You are old enough");
+} else {
+console. log( "go to sleep");
+}
+
+// do you have a dog
+var doYouHaveADog = true;
+if (doYouHaveADog) {
+    console.log ("Yes you have a dog");
+} else {
+    console.log ("other");
+}
